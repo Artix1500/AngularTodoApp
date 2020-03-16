42cfecd8a61ff1574e103cae6c06fdac1c97a616
Artix1500 <artur.haczek23@gmail.com>
2020-03-16 16:19:35 +1100

    Setup navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './Containers/dashboard/dashboard.component';
+import { AddComponent } from './Containers/add/add.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'add', component: AddComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
